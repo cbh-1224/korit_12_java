baf99afb807a54c4ec82e9ac06da4d5f06a5e8db
cbh-1234 <tmek1234@naver.com>
2025-12-22 17:46:20 +0900

    feat: 20251222 Java Lesson 시작

--- src/main/java/ch00_starting/Main.java
+++ src/main/java/ch00_starting/Main.java
@@ -3,5 +3,13 @@ package ch00_starting;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("최병현");
+        System.out.println("123");
+        System.out.println('1');
+        System.out.println("12");
+//        System.out.println(123456);
+//        System.out.println(hello);
+
+
     }
 }

1601390c87b7e140290151557705248c9dc373a3
cbh-1224 <tmek1234@naver.com>
2025-12-24 19:22:49 +0900

    Update Method03ScoreCalc1.java

--- src/main/java/ch07_methods/Method03ScoreCalc1.java
+++ src/main/java/ch07_methods/Method03ScoreCalc1.java
@@ -2,29 +2,39 @@ package ch07_methods;
 
 import java.util.Scanner;
 
-public class Method03ScoreCalc1 {
-    // 메서드 정의 영역 - Method02에 있던 기능을 메서드로 정의할겁니다.
-    // call1()으로 할겁니다.
-    public static void calcSumAndAvg() {
+public class Method03ScoreCalc2 {
+    // method 정의 영역 - 이전의 메서드03에서 정의한 방식은 하나의 메서드에 총합을 구하는 메서드와
+    // 평균을 구하는 메서드가 합쳐져 있었습니다.
+
+    // 이번 단계에서는 1. 합을 구하는 메서드 / 2. 평균을 구하는 메서드를 작성하고
+    // 평균을 구하는 메서드 내에서 합을 구하는 메서드를 호출하는 방식으로 작성할겁니다.
+
+    // 그리고 메서드 호출 영역에는 '평균을 구하는 메서드'만 호출할겁니다.
+
+    // 1. 합을 구하는 메서드
+    public static double calcSum() {
+        double sum = 0;
         Scanner scanner = new Scanner(System.in);
-        double sum=0;     // 서술형에 소수점이 많아서 그냥 이렇게 했습니다
-        int numOfSubs;
-        System.out.print("몇 과목의 점수를 입력하시겠습니까? >>> ");
-        numOfSubs = scanner.nextInt();
+        System.out.print("몇 개의 과목을 입력하시겠습니까? >>> ");
+        int numOfSubs = scanner.nextInt();
         for ( int i = 0 ; i < numOfSubs ; i++ ) {
-            System.out.print((i+1) + " 과목의 점수를 입력하세요 >>> ");
-            sum+= scanner.nextDouble();
+            System.out.print((i+1) + " 과목의 점수를 입력하세요. >>> ");
+            sum += scanner.nextDouble();
         }
-        System.out.println("총합은 " + sum + "이며, 평균은 " + (sum/numOfSubs) + "입니다.");
+        return sum;
     }
 
+    // 2. 평균을 구하는 메서드
+    public static void calcAvg() {
+        // 여기서 스캐너로 과목 수를 입력 받습니다.
+        double sum = calcSum(); // 그리고 합 구할 때 여기에 argument로 과목 수를 보내줘야 합니다. -> 그러면 정의 영역도 수정해야함
+        // 그리고 return으로 나온 합을 가지고 sum / 과목수 를 해서 평균을 구하면 답이 나올 것 같습니다.
+
+    }
+
+
     public static void main(String[] args) {
-        // 메서드 호출 영역
-
-        for (int i = 0 ; i < 100 ; i++ ) {
-            calcSumAndAvg();
-
-        }
-
+        // method 호출 영역
+        calcAvg();
     }
 }
